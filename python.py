cd1a1857e46d7b4ed203f6cccab279c8fc72908f
Anjana <anjanavijay2005@gmail.com>
2025-05-18 21:49:26 +0530

    pr

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ num1 = int(input('Enter first number: '))
 num2 = int(input ('Enter second number: '))
 
 # Add two numbers
-sum = (num1 + num2) 
+sum = (num1 + num3) 
 
 # Display the sum
 print('The sum of',num1,'and',num2,'is',sum)
